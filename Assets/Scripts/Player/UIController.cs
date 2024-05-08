0222e50e714f8830af3589fed40b4968d0fb0a2a
SammyScared <sam.yi.xie@gmail.com>
2024-05-07 21:32:31 -0700

    UI Controller finished as for item pickup

---
+++ Assets/Scripts/Player/UIController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    public int bombup = 0;
+    public int speedup = 0;
+    public int blastup = 0;
+    public TextMeshProUGUI bombupText;
+    public TextMeshProUGUI speedupText;
+    public TextMeshProUGUI blastupText;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddItem(ItemPickup.ItemType type)
+    {
+        switch (type)
+        {
+            case ItemPickup.ItemType.ExtraBomb:
+                bombup++;
+                UpdateItemCountUI(bombupText, bombup);
+                break;
+            case ItemPickup.ItemType.BlastRadius:
+                blastup++;
+                UpdateItemCountUI(blastupText, blastup);
+                break;
+            case ItemPickup.ItemType.SpeedIncrease:
+                speedup++;
+                UpdateItemCountUI(speedupText, speedup);
+                break;
+        }
+    }
+
+    void UpdateItemCountUI(TextMeshProUGUI uiText, int count)
+    {
+        uiText.text = count.ToString();
+    }
+}
