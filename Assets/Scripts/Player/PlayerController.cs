0628c25cfd63c226ce946eda4d9cf6031147276d
Piratelle <eratelle@berkeley.edu>
2024-05-09 04:17:57 -0700

    Continued branch alignment, player movement and animation.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,81 +1,61 @@
 /* Originally from David, modified by Jonah
+ * Modified by Erin to combine David's PlayerMovement and MovementController
  * Player controller script
  * - Handles movement
  * - Death sequence
  */ 
 
-
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Unity.Netcode;
 
 public class PlayerController : NetworkBehaviour
 {
     #region Variables
-    //private int playerNum;
-    private NetworkVariable<int> playerNum = new NetworkVariable<int>(0);
-    // Network variable for playercount
-    private Color[] playerColors = { Color.white, Color.red, Color.blue, Color.yellow };
+    //private NetworkVariable<int> playerNum = new NetworkVariable<int>(0);
+    //private Color[] playerColors = { Color.white, Color.red, Color.blue, Color.yellow };
 
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     [SerializeField] private GameObject camHolder;
-    private Vector2 direction = Vector2.down;
     [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 direction = Vector2.down;
 
     public KeyCode inputUp = KeyCode.W;
     public KeyCode inputLeft = KeyCode.A;
     public KeyCode inputDown = KeyCode.S;
     public KeyCode inputRight = KeyCode.D;
 
-    public Animator animator;
     public bool idle;
     private bool isDead;
 
     public AudioSource deathAudioSource;
-    public AudioClip deathSound;
+    //public AudioClip deathSound;
     #endregion
 
     #region Initialization
     public override void OnNetworkSpawn() 
     {
         base.OnNetworkSpawn();
-        if (IsServer)
-        {
-            playerNum.Value = ClientConnectionHandler.GetPlayerNum(OwnerClientId) + 1;
-            Debug.Log("I'm Player #" + playerNum.Value);
-        }
         if (!IsOwner) {
-            this.enabled = false;
-            this.camHolder.SetActive(false);
+            enabled = false;
+            camHolder.SetActive(false);
             return;
         }
         Initialize();
     }
 
-    // public void Start() {
-    //     if (rb == null) {
-    //         this.enabled = true;
-    //         Initialize();
-    //     }
-    // }
-
     private void Initialize() {
+        // get components
         deathAudioSource = GetComponent<AudioSource>();
-        deathSound = GetComponent<AudioClip>();
+        //deathSound = GetComponent<AudioClip>();
         rb = GetComponent<Rigidbody2D>();
         rb.bodyType = RigidbodyType2D.Dynamic; // fix for annoying collision-breaking bug of doom
+
         // attach main camera to player if owner (can also just set positions in update if works better)
         Camera.main.transform.SetParent(rb.transform);
 
-        // Set player color
-        GetComponent<SpriteRenderer>().color = playerColors[playerNum.Value - 1];
-        if (!IsHost) {
-            // playerNum = playerCount;
-            // GetComponent<SpriteRenderer>().color = color.red;
-        } else {
-            // Is host
-            //playerNum = 1;
-        }
+        // determine player data here!
     }
     #endregion
 
@@ -83,11 +63,13 @@ public class PlayerController : NetworkBehaviour
     private void Update()
     {
         // Input handling
-        if (Input.GetKey(inputUp)) direction = Vector2.up;
-        else if (Input.GetKey(inputDown)) direction =  Vector2.down;
-        else if (Input.GetKey(inputLeft)) direction = Vector2.left;
-        else if (Input.GetKey(inputRight)) direction = Vector2.right;
-        else direction = Vector2.zero;
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+
+        // Update animator
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Speed", direction.sqrMagnitude);
     }
 
     private void FixedUpdate() 
@@ -114,18 +96,17 @@ public class PlayerController : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     private void DeathSequenceServerRpc()
     {
-        this.enabled = false;
+        enabled = false;
         GetComponent<BombController>().enabled = false;
 
         // Death animation
 
         // Play the death sound
-        if (deathAudioSource != null && deathSound != null)
+        if (deathAudioSource != null) // && deathSound != null)
         {
-            deathAudioSource.clip = deathSound;
-            this.deathAudioSource.Play();
+            //deathAudioSource.clip = deathSound;
+            deathAudioSource.Play();
         }
-
         Invoke(nameof(OnDeathSequenceEnded), 1.25f);
     }
 
@@ -134,9 +115,6 @@ public class PlayerController : NetworkBehaviour
         gameObject.SetActive(false);
 
         // TODO: check win conditions, respawn if necessary
-
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        //GameManager.Instance.CheckWinState();
     }
     #endregion
 
@@ -150,7 +128,8 @@ public class PlayerController : NetworkBehaviour
     }
 
     public int GetPlayerNum() {
-        return playerNum.Value;
+        //return playerNum.Value;
+        return 0;
     }
     #endregion
 
